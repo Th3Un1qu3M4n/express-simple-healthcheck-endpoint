9a007f4ecca3ea9595365d98f2081136bfd9377f
Muhammad Ahmed <ahmed495802@gmail.com>
2023-10-27 01:57:18 +0500

    changed endpoint

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -4,7 +4,7 @@
 # Check if your Node.js server is running by sending an HTTP request or another method
 # For example, you can use curl to check if the server is responsive.
 # Replace 'http://localhost:3000' with the actual URL of your application.
-if curl -s --head --request GET http://localhost:3000 | grep "200 OK" > /dev/null; then
+if curl -s --head --request GET http://localhost:3000/healthcheck | grep "200 OK" > /dev/null; then
   echo "Validation successful: The Node.js service is running."
   exit 0
 else
